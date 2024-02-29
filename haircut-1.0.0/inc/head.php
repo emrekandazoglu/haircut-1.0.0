464a73c571a0af91ad938a032d7d84d7ae7e56e6
emrekandazoglu <114920764+emrekandazoglu@users.noreply.github.com>
2024-02-29 21:05:08 +0300

    first commit

---
+++ haircut-1.0.0/inc/head.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="utf-8">
+    <title>HairCut - Hair Salon HTML Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Oswald:wght@600&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-secondary navbar-dark sticky-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
+     <a href="index.php" class="navbar-brand ms-4 ms-lg-0">
+    <img src="img\site_logosu.png" alt="MakinePlastik Logo" class="logo-img">
+    <h1 class="mb-0 text-primary text-uppercase">Makine<span style="color:#6C7293 ;">Plastik</span></h1>
+    <style>
+        .navbar-brand {
+            display: flex;
+            align-items: center;
+        }
+
+        .logo-img {
+            width: 100px; /* Logonun genişliği */
+            height: auto; /* Otomatik yükseklik ayarı */
+            margin-right: 10px; /* Yazı ile logo arasındaki boşluk */
+        }
+    </style>
+</a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.php" class="nav-item nav-link ">Home</a>
+                <a href="about.php" class="nav-item nav-link">About</a>
+                <a href="service.php" class="nav-item nav-link">Service</a>
+                
+                <a href="contact.php" class="nav-item nav-link">Contact</a>
+                <a href="whatsapp://send?phone=+905511393700" class="nav-item nav-link" style="color: orangered;">Appointment</a>
+            </div>
+            
+        </div>
+    </nav>
+    <!-- Navbar End -->
